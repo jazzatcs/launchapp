d6e6950fa50c8f4fe0d2805d9efe4845831e3b8a
Jonathan Piper <jonathan.piper@contentstack.com>
2023-07-18 14:40:18 -0700

    init

--- src/app/custom-field/page.js
+++ src/app/custom-field/page.js
@@ -1,3 +1,12 @@
-export default function Page() {
-    return <p>hi</p>
+import ContentstackAppSDK from "@contentstack/app-sdk"
+
+async function getData () {
+    ContentstackAppSDK.init().then(async (appSDK) => {
+        return appSDK;
+    })
+}
+
+export default async function Page() {
+    const data = await getData();
+    return <p>{data}</p>
 }
