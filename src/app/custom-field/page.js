8ac60d3733accf3507d5787fca1e60945fe0eea0
Jonathan Piper <jonathan.piper@contentstack.com>
2023-07-18 15:07:34 -0700

    init

--- src/app/custom-field/page.js
+++ src/app/custom-field/page.js
@@ -1,12 +1,22 @@
-import ContentstackAppSDK from "@contentstack/app-sdk"
+'use client';
+import dynamic from 'next/dynamic';
+import { useEffect } from 'react';
 
 async function getData () {
-    ContentstackAppSDK.init().then(async (appSDK) => {
-        return appSDK;
+    // const [data, setData] = useState();
+    let data = '';
+    useEffect(() => {
+        const ContentstackAppSDK = dynamic(() => import('@contentstack/app-sdk'));
+        ContentstackAppSDK.init().then(async (appSDK) => {
+            const cFO = appSDK.location.CustomField;
+            const cFD = await cFO.field.getData();
+            data = cFD;
+        })
     })
+    return data;
 }
 
 export default async function Page() {
     const data = await getData();
-    return <p>{data}</p>
+    return <p>hi{data}</p>
 }
