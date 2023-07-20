ed4b0c39453a4c6af6c45605714e8789ee1a6881
Jonathan Piper <jonathan.piper@contentstack.com>
2023-07-20 11:21:28 -0700

    init

--- src/app/custom-field/page.js
+++ src/app/custom-field/page.js
@@ -1,23 +1,35 @@
-'use client';
-import dynamic from 'next/dynamic';
-import { useEffect } from 'react';
+"use client";
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 
-async function getData () {
-    // const [data, setData] = useState();
-    let data = '';
+async function getData() {
+    const [groups, setGroups] = useState([]);
+    const [customField, setCustomField] = useState();
+    const [config, setConfig] = useState();
+    let data = "";
     useEffect(async () => {
-        const ContentstackAppSDK = (await import('@contentstack/app-sdk')).default;
+        const ContentstackAppSDK = (await import("@contentstack/app-sdk"))
+            .default;
         ContentstackAppSDK.init().then(async (appSDK) => {
-            const cFO = await appSDK.location.CustomField;
-            const cFD = await cFO.field.getData();
-            data = cFD;
-            console.log(appSDK)
-        })
-    })
+            setConfig(await appSDK.getConfig());
+            if (config && Object.keys(config).length === 0) {
+                console.log(`No configuration available`);
+            }
+            const customFieldObject = await appSDK.location.CustomField;
+            setCustomField(customFieldObject);
+            var fieldData = await customFieldObject.field.getData();
+            const fieldConfig = await customFieldObject.fieldConfig;
+            if (!isEmpty(fieldData) && fieldData !== null) {
+                if (fieldData.constructorGroups) {
+                    setGroups(fieldData.constructorGroups);
+                }
+            }
+        });
+    });
     return data;
 }
 
 export default async function Page() {
     const data = await getData();
-    return <p>hi{data}</p>
+    return <p>hi{data}</p>;
 }
